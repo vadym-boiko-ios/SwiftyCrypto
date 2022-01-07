a86a46a2a073a9d941daa810d6abbcb9422c239c
Vadym Boiko <vadym.boikoo@gmail.com>
2022-01-07 23:49:42 +0200

    Create a view to manager current user's portfolio

---
+++ SwiftyCrypto/Core/Components/XMark.swift
@@ -0,0 +1,16 @@
+//
+//  XMarkButton.swift
+//  SwiftyCrypto
+//
+//  Created by Vadym Boiko on 07.01.2022.
+//
+
+import SwiftUI
+
+struct XMark: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.headline)
+    }
+}
+
